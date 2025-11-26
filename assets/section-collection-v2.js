b94a9a5cc81d6e4d562c45a81f5a0728ec2394fb
adityaprakash-boop <aditya.prakash@ecomexperts.io>
2025-11-27 02:32:06 +0530

    Working on the JS of Filters

--- assets/section-collection-v2.js
+++ assets/section-collection-v2.js
@@ -19,6 +19,19 @@ class CollectionFilter extends HTMLElement {
         });
       });
     }
+    updateClearButton(params) {
+        const btn = this.querySelector(".clear-all-btn");
+        if (params && params.length > 0) {
+          btn.style.display = "inline-flex";
+        } else {
+          btn.style.display = "none";
+        }
+        btn.onclick = (e) => {
+          e.preventDefault();
+          this.fetchSection(""); 
+        };
+      }
+      
     fetchSection(params) {
       const url = `${window.location.pathname}?section_id=${this.sectionId}&${params}`;
       fetch(url)
@@ -34,6 +47,7 @@ class CollectionFilter extends HTMLElement {
           const newUrl = params ? `${window.location.pathname}?${params}` : window.location.pathname;
           history.replaceState({}, "", newUrl);
           this.bindFilterLinks();
+          this.updateClearButton(params);
         })
         .catch(err => console.error("AJAX error:", err));
     }
